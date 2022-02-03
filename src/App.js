bc9a7f8200f7d60878229528a539a03af3ff8da3
JordanPessman <JordanPessman@users.noreply.github.com>
2022-02-03 09:53:58 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>PokePackz!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <ul>
+        <li>Home</li>
+        <li>Packs</li>
+        <li>About</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+}
+export default Nav;
